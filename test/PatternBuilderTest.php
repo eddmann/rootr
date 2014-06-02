1f210de79ea2c6564a61a1a6c99a65a243e9bb0d
Edward Mann <the@eddmann.com>
2014-06-02 12:28:09 +0100

    Changed setUp access modifier.

--- test/PatternBuilderTest.php
+++ test/PatternBuilderTest.php
@@ -6,7 +6,7 @@ class PatternBuilderTest extends \PHPUnit_Framework_TestCase
 
     protected $instance;
 
-    public function setUp()
+    protected function setUp()
     {
         $this->instance = new PatternBuilder;
     }
